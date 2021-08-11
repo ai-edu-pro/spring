1c691e29479460d1ea3cfe67297018277949b2f2
hjk7902 <hjk7902@gmail.com>
2021-08-11 10:43:30 +0900

    JDBC Sample Project 시작파일

---
+++ JDBCSample_start/src/main/java/com/coderby/myapp/hr/controller/EmpController.java
@@ -0,0 +1,31 @@
+package com.coderby.myapp.hr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.coderby.myapp.hr.model.EmpVO;
+import com.coderby.myapp.hr.service.IEmpService;
+
+@Controller
+public class EmpController {
+
+	@Autowired
+	IEmpService empService;
+	
+	@RequestMapping(value="/hr/count")
+	public String empCount(@RequestParam(value="deptid", required=false, defaultValue="0") int deptid, Model model) {
+		if(deptid==0) {
+			model.addAttribute("count", empService.getEmpCount());
+		}else {
+			model.addAttribute("count", empService.getEmpCount(deptid));
+		}
+		return "hr/count";
+	}
+
+}//end class
