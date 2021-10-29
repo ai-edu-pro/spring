256187557e84cfdbcf0ca37db9de1b4446442445
hjk7902 <hjk7902@gmail.com>
2021-10-29 11:14:54 +0900

    처음 시작하는 스프링 프레임워크 [2021 개정판]

---
+++ SpringBook_Chapter2_AOP(XML)/src/main/java/com/coderby/myapp/aop/HelloController.java
@@ -0,0 +1,15 @@
+package com.coderby.myapp.aop;
+
+public class HelloController {
+	
+	IHelloService helloService;
+	
+	public void setHelloService(IHelloService helloService) {
+		this.helloService = helloService;
+	}
+
+	public void hello(String name) {
+		String result = helloService.sayHello(name);
+		System.out.println(result);
+	}
+}
---
+++ SpringBook_Chapter2_AOP(XML)/src/main/java/com/coderby/myapp/aop/IHelloService.java
@@ -0,0 +1,6 @@
+package com.coderby.myapp.aop;
+
+public interface IHelloService {
+	String sayHello(String name);
+	String sayGoodbye(String name);
+}
